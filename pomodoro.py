783b186d0431830a37469c32b1fafbd0131c3cf9
Prabesh Subedi <65777903+pravuX@users.noreply.github.com>
2021-11-15 21:54:28 +0545

    Add session prompt on session end

--- pomodoro.py
+++ pomodoro.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+
 import sys
 import os
 import re
 from time import sleep
 from notifypy import Notify
+
 import add_sessions
 
 clear = lambda: os.system("clear")
@@ -13,15 +15,13 @@ play = lambda: os.system(f"mpv --really-quiet {alarm_clock}")
 
 def getTime(time):
         # time is provided in seconds and returned as mm:ss
-        mins = str(time // 60)
-        secs = str(time % 60)
-        fmt_min = "0" + mins if len(mins) == 1 else mins
-        fmt_sec = "0" + secs if len(secs) == 1 else secs
-        return f"{fmt_min}:{fmt_sec}"
+        mins = time // 60
+        secs = time % 60
+        return f"{mins:02d}:{secs:02d}"
 
 def notify(type):
-    # type is the type of notification to send i.e. Break or Session
-    sleep(0.5)
+    # type is the type of notification to send i.e. Break or Session.
+    print(f"{type} Completed", end="\r")
     notification = Notify()
     notification.title = "PoPy - Notify"
     notification.message = f"{type} Completed"
@@ -34,31 +34,66 @@ def timer(set_session, type):
     print(f"Starting {type}... ")
     sleep(0.5)
     for i in range(int(set_session)*60, 0, -1):
-        clear()
-        print(getTime(i))
+        print(getTime(i), end="\r") # overwrites previous line
         sleep(1)
     clear()
     notify(type)
 
-# Exactly one argument must be provided
-    # and in the proper format for the program to work
-if 1 < len(sys.argv) < 3:
-    # Set necessary variables
-    set_time = sys.argv[1]
+def start_pomdoro_session(set_time):
     set_session = re.split(r"\+", set_time)[0]
     set_break = re.split(r"\+", set_time)[1]
-    # Then start the session
+    # Start the session
     timer(set_session, "Session")
+    # Then start the break.
+    start_break(set_break)
+    # Update session counter.
     add_sessions.update_or_add_pomodoro_count()
+
+
+def start_prompt():
+    while True:
+        session_info = ''
+        input_pattern = re.compile(r"\d+\+\d+")
+        # Keep asking for session info untill right info is provided
+        while not input_pattern.match(session_info):
+            print("Enter Session Info: ")
+            session_info = input("> ")
+        start_pomdoro_session(session_info)
+        print("Start another session? (yes or no)")
+        ans = input("> ")
+        # Break out of the loop is answer is no.
+        if not ans.lower().startswith('y'):
+            break
+
+def start_break(set_break):
     # Prompt user for break
     print("Start Break(type yes, or no): ")
-    ans = input()
-    if ans == "yes":
+    ans = input("> ")
+    if ans.lower().startswith("y"):
         timer(set_break, "Break")
-    sys.exit()
-else:
-    print(
-"""Invalid Argument. Please specify a session in the following format.
-session_length+break_length
-For example: 25+5, 90+10""")
-    sys.exit()
+
+
+def main():
+    """Exactly one argument must be provided and in the proper format for the program to work"""
+    arg_pattern = re.compile(r"\d+\+\d+")
+    if 1 < len(sys.argv) < 3 and arg_pattern.match(sys.argv[1]):
+        # Time provided by user as argument.
+        set_time = sys.argv[1]
+        # Start the session.
+        start_pomdoro_session(set_time)
+        # After the break, prompt for new session.
+        print("Start a new session? (yes or no)")
+        ans = input("> ")
+        # Exit if answer is not yes.
+        if not ans.lower().startswith('y'):
+            sys.exit()
+        # Else start the prompt.
+        start_prompt()
+    else:
+        print(
+    """Invalid Argument. Please specify a session in the following format.
+    session_length+break_length
+    For example: 25+5, 90+10""")
+
+if __name__ == "__main__":
+    main()
